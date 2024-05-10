01a8bfab482fccb8165fc624c810001eea1e54cb
briheet01 <briheetyadav@gmail.com>
2024-05-10 19:58:40 +0530

    refactoring

--- linux/linux.go
+++ linux/linux.go
@@ -1,21 +1,11 @@
 package linux
 
-type Parameters struct {
-	Name       string
-	Host       string
-	Kernel     string
-	Uptime     string
-	Packages   string
-	Shell      string
-	Resolution string
-	WM         string
-	Theme      string
-	Icons      string
-	Terminal   string
-	CPU        string
-	GPU        string
-	Memory     string
-}
+import "runtime"
 
 func GetInfo(*Parameters, error) {
+	operatingSystemInfo := new(Parameters)
+
+	systemName := runtime.GOOS
+	if systemName != "linux" {
+	}
 }
---
+++ os/os_info.go
@@ -0,0 +1,18 @@
+package os
+
+type Parameters struct {
+	Name       string
+	Host       string
+	Kernel     string
+	Uptime     string
+	Packages   string
+	Shell      string
+	Resolution string
+	WM         string
+	Theme      string
+	Icons      string
+	Terminal   string
+	CPU        string
+	GPU        string
+	Memory     string
+}
