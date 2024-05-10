921035153a929875b50ffb72d828298156a4f7a5
briheet01 <briheetyadav@gmail.com>
2024-05-10 14:20:31 +0530

    restruct

--- src/main.go
+++ src/main.go
@@ -3,28 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
-)
 
-type Parameters struct {
-	Name       string
-	Host       string
-	Kernel     string
-	Uptime     string
-	Packages   string
-	Shell      string
-	Resolution string
-	WM         string
-	Theme      string
-	Icons      string
-	Terminal   string
-	CPU        string
-	GPU        string
-	Memory     string
-}
+	"github.com/briheet/gofetch/linux"
+)
 
 func main() {
 	now := time.Now()
-	operatingSystemInfo := GetInfo()
+	operatingSystemInfo := linux.GetInfo()
 
 	fmt.Println(now)
 }
