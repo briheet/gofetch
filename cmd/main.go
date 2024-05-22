76087d18989c9d855ebccfe202559837557c0ce5
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-05-22 16:05:00 +0530

    added support for system type
    added for linux just now dont have implementation for darwin or windows

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,38 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"runtime"
 
+	"github.com/briheet/gofetch/fetch"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	app := (&cli.App{
-		Name:  "gofetch",
-		Usage: "fetching the system information",
-		Action: func(*cli.Context) error {
-			fmt.Println("hi")
-			return nil
+	app := &cli.App{
+		Commands: []*cli.Command{
+			newVersionCommand(),
 		},
-	})
+		Name:   "gofetch",
+		Usage:  "fetching the system information",
+		Action: FetchVersion,
+	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func FetchVersion(cli *cli.Context) error {
+	var os fetch.Fetch
+
+	switch goos := runtime.GOOS; goos {
+	case "linux":
+		os = linux.New()
+	}
+
+	fetch.Fetch(os)
+
+	return nil
 }
--- fetch/output.go
+++ fetch/output.go
@@ -1,1 +1,12 @@
 package fetch
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+func GetTheInformation(cli *cli.Context) error {
+	fmt.Println("hi")
+	return nil
+}
